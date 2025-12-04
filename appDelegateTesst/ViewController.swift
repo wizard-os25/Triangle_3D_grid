103ca58223a21219cf5514ce8f75096ed43c8bab
the_wizard <wizard.os25@proton.me>
2025-12-04 10:13:43 +0700

    Initial Commit

---
+++ appDelegateTesst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appDelegateTesst
+//
+//  Created by wizard.os25 on 4/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
